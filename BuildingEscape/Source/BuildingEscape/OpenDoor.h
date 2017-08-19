2191a8a861176d25e28ed56c6ee788885207b817
Matej <poliacek.matej@gmail.com>
2017-08-19 17:11:06 +0100

    Added TriggerVolume basics

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine.h"
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
@@ -19,11 +20,15 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-
+	
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
 	
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
 };
